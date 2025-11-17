cae9e1b8a22d13c34d6db18eb5e2d1a0f6680b72
Your name <amraouim3il.fr>
2025-11-17 16:11:06 +0100

    Victooooor

--- src/MessageB.java
+++ src/MessageB.java
@@ -1,6 +1,7 @@
 
 public class MessageB {
  public static void main(String[] args) {
-	 System.out.println("Message du Membre B ");
+	   System.out.println("Message du membre B"); 
+       System.out.println("Deuxième ligne de B Victoooooor"); 
  }
 }
